a7b0ddcdcf21d70a2f3734043a9b14055d10ea8c
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-11-11 11:24:27 +0100

    fix: update default port to 4859 in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 4859;
 
 app.use(express.json());
 
